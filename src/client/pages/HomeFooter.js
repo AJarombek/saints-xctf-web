bbfa71170451967001dd63daf4d390ca7be1f3d5
Andrew Jarombek <ajarombek95@gmail.com>
2020-01-23 23:06:18 -0500

    further home page styling

--- src/client/pages/HomeFooter.js
+++ src/client/pages/HomeFooter.js
@@ -5,11 +5,15 @@
  */
 
 import React from 'react';
+import saints_xctf_logo from '../../assets/saintsxctf_logo.png';
 
 const HomeFooter = () => {
     return (
         <div className="sxctf-home-footer">
           <a href="mailto:andrew@jarombek.com">Contact Us</a>
+          <figure className="sxctf-logo">
+            <img src={saints_xctf_logo} />
+          </figure>
         </div>
     );
 };
