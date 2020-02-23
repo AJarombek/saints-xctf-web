e40821c39f26b222868e86726ff0fe14b9b59233
Andrew Jarombek <ajarombek95@gmail.com>
2020-02-23 17:10:53 -0500

    begin styling mobile navbar

--- src/client/home/HomeNavBar.js
+++ src/client/home/HomeNavBar.js
@@ -14,33 +14,50 @@ const HomeNavBar = () => {
   const history = useHistory();
   const [showDropdown, setShowDropdown] = useState(false);
 
+  const navBarClass = showDropdown ?
+    classnames('sxctf-home-nav-bar', 'sxctf-home-nav-bar-dropdown-visible') :
+    classnames('sxctf-home-nav-bar');
+
   const dropdownClass = showDropdown ?
     classnames('sxctf-nav-dropdown', 'sxctf-nav-dropdown-visible') :
     classnames('sxctf-nav-dropdown');
 
   return (
-    <div className="sxctf-home-nav-bar">
-      <figure className="sxctf-logo">
-        <img src={saints_xctf_logo}/>
-      </figure>
-      <h1>SaintsXCTF</h1>
-      <div className="sxctf-nav-buttons">
-        <AJButton type="text">About</AJButton>
-        <AJButton type="text">Testimonials</AJButton>
-        <AJButton type="outlined">Sign Up</AJButton>
-        <AJButton type="contained">Log In</AJButton>
-      </div>
-      <div className="sxctf-nav-hamburger">
-        <AJMobileHamburger onClick={() => setShowDropdown(!showDropdown)}/>
+    <>
+      <div className={navBarClass}>
+        <figure className="sxctf-logo">
+          <img src={saints_xctf_logo}/>
+        </figure>
+        <h1>SaintsXCTF</h1>
+        <div className="sxctf-nav-buttons">
+          <AJButton type="text" onClick={() => history.push('/#about')}>
+            About
+          </AJButton>
+          <AJButton type="text" onClick={() => history.push('/#testimonials')}>
+            Testimonials
+          </AJButton>
+          <AJButton type="outlined" onClick={() => history.push('/signup')}>
+            Sign Up
+          </AJButton>
+          <AJButton type="contained" onClick={() => history.push('/login')}>
+            Log In
+          </AJButton>
+        </div>
+        <div className="sxctf-nav-hamburger">
+          <AJMobileHamburger onClick={() => setShowDropdown(!showDropdown)}/>
+        </div>
       </div>
       <div className={dropdownClass}>
         <AJNavList
           items={[
-            {text: 'About', onClick: () => history.push('/#about')}
+            {text: 'About', onClick: () => history.push('/#about')},
+            {text: 'Testimonials', onClick: () => history.push('/#testimonials')},
+            {text: 'Sign Up', onClick: () => history.push('/signup')},
+            {text: 'Log In', onClick: () => history.push('/login')}
           ]}
         />
       </div>
-    </div>
+    </>
   );
 };
 
