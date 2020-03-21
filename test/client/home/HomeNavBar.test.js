4409caec550d232c5733a7e0149be4ddb2bf3bc6
Andrew Jarombek <ajarombek95@gmail.com>
2020-03-21 11:26:45 -0400

    approaching full code coverage

--- test/client/home/HomeNavBar.test.js
+++ test/client/home/HomeNavBar.test.js
@@ -21,6 +21,10 @@ jest.mock('react-router-dom', () => {
   }
 });
 
+// I hope you are healthy and doing okay, and that your loved ones are doing the same.  I'm
+// guessing you are still able to spend this time with those you love,
+// which makes me happy for you :).
+
 describe('unit tests', () => {
 
   it('renders', () => {
@@ -39,8 +43,37 @@ describe('integration tests', () => {
     const websiteLogo = wrapper.find('.sxctf-logo').childAt(0);
     const websiteTitle = wrapper.find('.sxctf-home-nav-bar').childAt(1);
 
+    const navButtons = wrapper.find('.sxctf-nav-buttons');
+
+    /**
+     * Helper function to get a navigational button on the desktop website header.
+     * @param index Index of the button as a child of sxctf-nav-buttons in the HTML.
+     * @return {ShallowWrapper|ReactWrapper} The element in the button that has a click
+     * listener attached.
+     */
+    const getNavButton = (index) => {
+      return navButtons.childAt(index).childAt(0).childAt(0).childAt(0);
+    };
+
+    const aboutButton = getNavButton(0);
+    const testimonialButton = getNavButton(1);
+    const signupButton = getNavButton(2);
+    const loginButton = getNavButton(3);
+
+    const mobileAboutNav = wrapper.find('.aj-nav-list-item').at(0).childAt(0);
+    console.info(mobileAboutNav.debug());
+    const mobileTestimonialsNav = wrapper.find('.aj-nav-list-item').at(1).childAt(0);
+    const mobileSignUpNav = wrapper.find('.aj-nav-list-item').at(2).childAt(0);
+    const mobileLogInNav = wrapper.find('.aj-nav-list-item').at(3).childAt(0);
+    const mobileLogoNav = wrapper.find('.aj-nav-list-item').at(4).childAt(0);
+
     expect(websiteLogo.type()).toEqual('img');
     expect(websiteTitle.type()).toEqual('h1');
+
+    expect(aboutButton.hasClass('aj-text-button')).toBe(true);
+    expect(testimonialButton.hasClass('aj-text-button')).toBe(true);
+    expect(signupButton.hasClass('aj-outlined-button')).toBe(true);
+    expect(loginButton.hasClass('aj-contained-button')).toBe(true);
 
     expect(pushSpy).not.toHaveBeenCalled();
     expect(pushSpy).toHaveBeenCalledTimes(0);
@@ -53,8 +86,52 @@ describe('integration tests', () => {
 
     websiteTitle.simulate('click');
 
-    expect(pushSpy).toHaveBeenCalled();
     expect(pushSpy).toHaveBeenCalledTimes(2);
+    expect(pushSpy).toHaveBeenCalledWith('#');
+
+    aboutButton.simulate('click');
+
+    expect(pushSpy).toHaveBeenCalledTimes(3);
+    expect(pushSpy).toHaveBeenCalledWith('/#about');
+
+    testimonialButton.simulate('click');
+
+    expect(pushSpy).toHaveBeenCalledTimes(4);
+    expect(pushSpy).toHaveBeenCalledWith('/#testimonials');
+
+    signupButton.simulate('click');
+
+    expect(pushSpy).toHaveBeenCalledTimes(5);
+    expect(pushSpy).toHaveBeenCalledWith('/signup');
+
+    loginButton.simulate('click');
+
+    expect(pushSpy).toHaveBeenCalledTimes(6);
+    expect(pushSpy).toHaveBeenCalledWith('/login');
+
+    mobileAboutNav.simulate('click');
+
+    expect(pushSpy).toHaveBeenCalledTimes(7);
+    expect(pushSpy).toHaveBeenCalledWith('/#about');
+
+    mobileTestimonialsNav.simulate('click');
+
+    expect(pushSpy).toHaveBeenCalledTimes(8);
+    expect(pushSpy).toHaveBeenCalledWith('/#testimonials');
+
+    mobileSignUpNav.simulate('click');
+
+    expect(pushSpy).toHaveBeenCalledTimes(9);
+    expect(pushSpy).toHaveBeenCalledWith('/signup');
+
+    mobileLogInNav.simulate('click');
+
+    expect(pushSpy).toHaveBeenCalledTimes(10);
+    expect(pushSpy).toHaveBeenCalledWith('/login');
+
+    mobileLogoNav.simulate('click');
+
+    expect(pushSpy).toHaveBeenCalledTimes(11);
     expect(pushSpy).toHaveBeenCalledWith('#');
   });
 
