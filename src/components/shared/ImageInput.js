7e0d58a965a0cabc88b228105eace431a7c882bf
Andrew Jarombek <ajarombek95@gmail.com>
2020-05-03 10:09:08 -0400

    begin sign in form

---
+++ src/components/shared/ImageInput.js
@@ -0,0 +1,53 @@
+/**
+ * Container component for an input element that has an identification icon and a validation icon.
+ * @author Andrew Jarombek
+ * @since 5/2/2020
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ImageInput = ({ type, name, placeholder, onChange, icon, status = 0, successIcon,
+                      failureIcon, warningIcon }) => {
+
+  let statusIcon;
+  switch (status) {
+    case SUCCESS:
+      statusIcon = successIcon;
+      break;
+    case WARNING:
+      statusIcon = warningIcon;
+      break;
+    case FAILURE:
+      statusIcon = failureIcon;
+      break;
+    default:
+      statusIcon = failureIcon;
+  }
+
+  return (
+    <div className="sxctf-image-input">
+      {icon && <img src={icon} alt />}
+      <input name={name} type={type} placeholder={placeholder} onChange={onChange} />
+      <img src={statusIcon} alt />
+    </div>
+  );
+};
+
+export const SUCCESS = 0;
+export const WARNING = 1;
+export const FAILURE = 2;
+
+ImageInput.propTypes = {
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  icon: PropTypes.any,
+  status: PropTypes.oneOf([SUCCESS, WARNING, FAILURE]),
+  successIcon: PropTypes.any,
+  failureIcon: PropTypes.any,
+  warningIcon: PropTypes.any
+};
+
+export default ImageInput;
--- src/components/sign-in/SignInBody.js
+++ src/components/sign-in/SignInBody.js
@@ -4,14 +4,31 @@
  * @since 5/2/2020
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { AJButton } from 'jarombek-react-components';
+import ImageInput from '../shared/ImageInput';
 
 const SignInBody = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="sxctf-sign-in-body">
       <h2>Sign In</h2>
-
+      <div>
+        <ImageInput
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username or Email"
+          name="username"
+          type="text"
+        />
+        <ImageInput
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          name="password"
+          type="password"
+        />
+      </div>
       <p>Forgot Password?</p>
       <div>
         <AJButton type="contained">Sign In</AJButton>
